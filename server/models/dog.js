b84c6594d67c0aa8006ea0a221f8efa54d1c20e0
alanbutcher <alan.butcher81@gmail.com>
2018-11-30 15:23:56 -0700

    count

--- server/models/dog.js
+++ server/models/dog.js
@@ -3,14 +3,10 @@ let Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId
 let name = "Dog"
 
-let count = new Schema({
-  user: { type: ObjectId, ref: 'User' },
-  vote: { type: Number, default: 0 }
-})
 
 let schema = new Schema({
   creatorId: { type: ObjectId, ref: "User", required: true }, //look at this if broken, not sure about ObjectId
-  counts: [count], 
+  count: { type: Number, default: 0, required: true }, 
   description: {
     breed: { type: String, required: true, default: "Mutt" },
     age: { type: Number }, 
