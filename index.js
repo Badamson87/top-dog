080c9cf127ecb58c55a896b9b963c93c3d2e5ee5
alanbutcher <alan.butcher81@gmail.com>
2018-11-30 10:46:35 -0700

    backend auth finished

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(express.static(__dirname + '/public'))
 
+let auth = require('./server/auth/routes')
+server.use(auth.session)
+server.use('/account', auth.router)
+
+//Allow users to get data when not logged in
+server.use("*", (req, res, next) => {
+  if (req.method == "GET") {
+    return next()
+  }
+  if (!req.session.uid) {
+    return next(new Error("Please login to continue"))
+  }
+  if (req.method == "POST") {
+    req.body.creatorId = req.session.uid
+  }
+  next()
+})
+//^^ above always the same
+
 
 
 
